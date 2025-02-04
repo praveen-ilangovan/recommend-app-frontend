8da986833e2f7701df0da6da0c0876efa2615dd7
Praveen Ilangovan <praveen.ilangovan@gmail.com>
2025-02-04 21:57:48 +0000

    Added an env file

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,2 +1,4 @@
-export const LOCAL_RECOMMEND_APP_URL = "http://127.0.0.1:8000";
-export const RECOMMEND_APP_URL = "https://recommend-app.onrender.com";
+// export const LOCAL_RECOMMEND_APP_URL = "http://127.0.0.1:8000";
+// export const RECOMMEND_APP_URL = "https://recommend-app.onrender.com";
+
+export const RECOMMEND_APP_URL = import.meta.env.VITE_APP_RECOMMEND_BASE_URL;
